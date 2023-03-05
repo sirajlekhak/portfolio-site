925e1e6b850a7ff9c1a00c230f1f692513a08253
Siraj Alam <crajmusics@gmail.com>
2023-03-06 00:37:00 +0530

    added portfolio site

---
+++ js/style-switcher.js
@@ -0,0 +1,44 @@
+// TOGGLE STYLE SWITCHER
+const styleswitcherToggle = document.querySelector(".style-switcher-toggler");
+styleswitcherToggle.addEventListener("click", () => {
+    document.querySelector(".style-switcher").classList.toggle("open");
+})
+//HIDE STYLE SWITCHER ON SCROLL
+window.addEventListener("wheel", (event) => {
+    if(document.querySelector(".style-switcher").classList.contains("open"))
+    {
+        document.querySelector(".style-switcher").classList.remove("open");
+    }
+})
+//THEME COLORS
+const alternateStyles = document.querySelectorAll(".alternate-style");
+function setActiveStyle(color)
+{
+    alternateStyles.forEach((style) => {
+        if(color === style.getAttribute("title"))
+        {
+            style.removeAttribute("disabled");
+        }
+        else
+        {
+            style.setAttribute("disabled" , "true");
+        }
+    })
+}
+//THEME LIGHT AND DARK MODE
+const dayNight = document.querySelector(".day-night");
+dayNight.addEventListener("click" , () => {
+    dayNight.querySelector("i").classList.toggle("fa-sun");
+    dayNight.querySelector("i").classList.toggle("fa-moon");
+    document.body.classList.toggle("dark");
+})
+window.addEventListener("load", () => {
+    if(document.body.classList.contains("dark"))
+    {
+        dayNight.querySelector("i").classList.add("fa-sun");
+    }
+    else
+    {
+        dayNight.querySelector("i").classList.add("fa-moon");
+    }
+})
